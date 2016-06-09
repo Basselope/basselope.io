023c0f4a9205f642b75d3f005423ff1997b7befc
Rico Chen <ricochenx@gmail.com>
2016-06-09 15:45:48 -0700

    [fix] add .jsx to import statements per webpack config

--- app/public/jsx/public_index.jsx
+++ app/public/jsx/public_index.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-//import searchReducer from './reducers/searchReducer.jsx'
-import App from './components/App.jsx'
+import searchReducer from './reducers/searchBarReducer.jsx'
+import App from './components/app.jsx'
 
-//let store = createStore(searchReducer, window.STATE_FROM_SERVER);
+let store = createStore(searchReducer, window.STATE_FROM_SERVER);
 
-render(
+render (
   <Provider store={store}>
     <App />
   </Provider>,
