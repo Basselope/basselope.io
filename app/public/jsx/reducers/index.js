5e309ad9193a25d62e5b4b505e21e87fddf1cfab
Rico Chen <ricochenx@gmail.com>
2016-06-08 17:35:55 -0700

    [design] file structure

---
+++ app/public/jsx/reducers/index.js
@@ -0,0 +1,8 @@
+// import { combineReducers } from 'redux'
+// import s from './todos'
+// import counter from './counter'
+
+// export default combineReducers({
+//   todos,
+//   counter
+// })
---
+++ app/public/jsx/reducers/keywordReducer.js
@@ -0,0 +1,12 @@
+// const INITIAL_STATE = { current: '', data: [] };
+
+// export default function keywordReducer(state = INITIAL_STATE, action) {
+//   switch(action.type) {
+//     case 'GET_KEYWORD': {
+//       return { ...state, current: action.data.data};
+//     }
+//     default: {
+//       return state;
+//     }
+//   }
+// }
