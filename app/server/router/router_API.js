1465358ca77742f865b1f513c767eaaaa07fd8c2
Ben Chen <coben88@gmail.com>
2016-06-09 09:40:46 -0700

    twitter api handed in

--- app/server/router/router_API.js
+++ app/server/router/router_API.js
@@ -2,14 +2,15 @@ var twitterHandle = require('../../../app/public/js/views/getTweets.js');
 var bingHandle = require('../../../app/public/js/views/getBing.js');
 
 function apihandlers(req,res,next){
-	console.log(req.url)
-
-	if(req.url=="/_api/twitter/search"){
+	console.log(req.url);
+	switch(req.url){
+		case '/_api/twitter/search':
 			req.body.query = "israel";
 			twitterHandle.getTweets(req, res, next)
-	}else{
-		bingHandle.getBing(req,res,next);
-	}			
+		case '/_api/bing/search':
+			bingHandle.getBing(req,res,next);
+
+	}		
 }
 
 
--- app/server/server_config.js
+++ app/server/server_config.js
@@ -25,65 +25,7 @@ app.get('/*', function(req, res, next) {
 });
 
 
-
-
-
-
-
-
-
-// var request = require('request');
-// var consumer_key = '9Daz6zUNQKhNYzogJD040b3r1';
-// var consumer_secret = 'sivAWFQ3r1MzfnvR1PULAoARrruhizjYpXb803jmx8YwvH4b1E';
-// var access_token = '3344662383-2Pgveoc6mHoUpbzXOwcwt5Nt6JPuEItsBjU8xXU';
-// var access_token_secret = 'q7e35cYUkgbMcuTIHmO6DpFotta37arrQ2z19ztkJt4M3';
-
-// var enc_secret = new Buffer(consumer_key + ':' + consumer_secret).toString('base64');
-
-// var oauthOptions = {
-//   url: 'https://api.twitter.com/oauth2/token',
-//   headers: {'Authorization': 'Basic ' + enc_secret, 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
-//   body: 'grant_type=client_credentials'
-// };
-
-// request.post(oauthOptions, function(e, r, body) {
-  
-// });
-
-
-
-// var twitterProxyServer = require('twitter-proxy');
-// twitterProxyServer({
-//   "consumerKey": consumer_key,
-//   "consumerSecret": consumer_secret,
-//   "accessToken": access_token,
-//   "accessTokenSecret": access_token_secret
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports = app;
-
-
-
-// app.post('/FILE', function(req, res) {
-//   DO SOMETHING(req, res);
-// })
 
 
 
