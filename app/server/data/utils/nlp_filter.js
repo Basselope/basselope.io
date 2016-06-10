c99e2f415239253708e87631109a5471b14d984e
Lukas Welinder <Lukas@Welinder.Co>
2016-06-10 15:58:36 -0700

    [design] started work on api data structures

--- app/server/data/utils/nlp_filter.js
+++ app/server/data/utils/nlp_filter.js
@@ -1,48 +1,10 @@
 'use strict';
 
 const nlp = reuquire('nlp_compromise');
-const _ = require('lodash')
-
 nlp.plugin(require('simple_english'));
 nlp.plugin(require('nlp-locale'));
 
-const twitter_user = (tweet) => ({
-  id: tweet.user.id,
-  img: tweet.user.profile_image_url || null,
-  name: tweet.user.screen_name,
-  handle: tweet.user.screen_name,
-  status_count: tweet.user.statuses_count,
-  friend_count: tweet.user.friends_count,
-  listed_count: tweet.user.listed_count
-});
+const _ = require('lodash')
 
-const twitter_author_struct = (user, src) => ({
-  author: {
-    twitter: {
-      id: tweet.user.id,
-      img: tweet.user.profile_image_url || null,
-      name: tweet.user.screen_name,
-      handle: tweet.user.screen_name,
-      status_count: tweet.user.statuses_count,
-      friend_count: tweet.user.friends_count,
-      listed_count: tweet.user.listed_count,
-    }
-  },
-  content: {
-    twitter: [],
-    reddit: [],
-  }
-});
+const struct = ('./api_struct.js');
 
-// {
-//   id: tweet.id_str,
-//     text: tweet.text,
-//   urls: tweet.entities.urls || null,
-//   hashtags: tweet.entities.hashtags || null,
-//   mentions_to: tweet.entities.user_mentions || null,
-//   responds_to: tweet.in_reply_to_user_id_str || null,
-//   fav_count: tweet.favorites_count,
-//   ret_count: tweet.retweet_count,
-//   location: tweet.coordinates,
-//   created_at: tweet.created_at
-// }
