ff3366cdc7fffd2934493a951b02567a275019c4
Rico Chen <ricochenx@gmail.com>
2016-06-10 17:27:13 -0700

    [feat] create get request handler for reddit API

--- app/server/router/router_API.js
+++ app/server/router/router_API.js
@@ -1,17 +1,20 @@
 var twitterHandle = require('./utils/getTweets.js');
 var bingHandle = require('./utils/getBing.js');
+var redditHandle = require('./utils/getReddit.js');
 
-function apihandlers(req,res,next){
+function apihandlers(req, res, next) {
 	console.log(req.url);
-	switch(req.url){
+	switch(req.url) {
 		case '/_api/twitter/search':
 			twitterHandle.getTweets(req, res, next);
 			break;
 		case '/_api/bing/search':
-			bingHandle.getBing(req,res,next);
+			bingHandle.getBing(req, res, next);
 			break;
-
-	}		
+    case '/_api/reddit/search':
+      redditHandler(req, res, next);
+      break;
+	}
 }
 
 
@@ -20,5 +23,3 @@ function apihandlers(req,res,next){
 
 var exports = module.exports = {};
 exports.routerHandler = apihandlers;
-
-
