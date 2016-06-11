010c0ee930a48fdd39b68329758fe13d77b2c2c2
Rico Chen <ricochenx@gmail.com>
2016-06-10 17:32:56 -0700

    [feat] create container for twitter

--- app/public/jsx/public_index.jsx
+++ app/public/jsx/public_index.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import searchReducer from './reducers/searchBarReducer.jsx'
+// import AppReducer from './reducers/reducers_index.jsx'
+import AppReducer from './reducers/tweets.jsx'
 import App from './components/app.jsx'
 
-let store = createStore(searchReducer, window.STATE_FROM_SERVER);
+let store = createStore(AppReducer, window.STATE_FROM_SERVER);
 
-render (
+render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('app')
-);
+)
