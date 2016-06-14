60c96001c3843ce074f6af42a87e455d8d6ca5a5
Lukas Welinder <Lukas@Welinder.Co>
2016-06-14 13:17:10 -0700

    [chore] code cleanup

--- app/server/router/router_API.js
+++ app/server/router/router_API.js
@@ -5,12 +5,11 @@ var twitterHandle = require('./utils/api/config/getTweets.js');
 var bingHandle = require('./utils/api/config/getBing.js');
 var redditHandle = require('./utils/api/config/getReddit.js');
 
-
 function apihandlers(req, res, next) {
 	console.log(req.url);
 	switch(req.url) {
 		case '/_api/twitter/search':
-			api_call("twitter", [req.body.query,'#'+req.body.query])
+			api_call('twitter', [req.body.query,'#'+req.body.query])
 				.then((ret) => res.status(200).send(ret))
 				.catch((err) => res.status(400).send(err));
 			break;
