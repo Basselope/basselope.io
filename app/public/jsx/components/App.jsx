5f7b1fa5d60d64a078728a9439a54a6b65b7b681
Ben Chen <coben88@gmail.com>
2016-06-15 19:42:50 -0700

    added hover react

--- app/public/jsx/components/App.jsx
+++ app/public/jsx/components/App.jsx
@@ -3,13 +3,16 @@ import SearchBar from '../containers/SearchBar.jsx'
 import TwitterGraph from '../containers/TwitterGraph.jsx'
 import RedditGraph from '../containers/RedditGraph.jsx'
 import InitialGraph from '../containers/InitialGraph.jsx'
+import HoverInfo from '../containers/HoverInfo.jsx'
 
 const App = () => (
   <div>
+  	  <HoverInfo />
     <SearchBar />
     <TwitterGraph />
     <RedditGraph />
     <InitialGraph />
+  
   </div>
 )
 
---
+++ app/public/jsx/containers/HoverInfo.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { connect } from 'react-redux'
+require('materialize-loader');
+
+class HoverInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {hover:false};
+    this.mouseOver = this.mouseOver.bind(this);
+    this.mouseOut = this.mouseOut.bind(this);
+  }
+
+  mouseOver (val) {
+    var ben = val;
+        this.setState({hover: true});
+    }
+    
+  mouseOut (val) {
+        this.setState({hover: false});
+  }
+
+  render() {
+        var position = {position:"absolute"};
+
+    var label = "";
+    var content = ""
+    var display = {display:"none", position:"absolute"}
+        if (this.state.hover) {
+            label = "Normal Graph";
+            content = "Graph displays sentiment of trending data against the sentiment's social impact";
+            display={display:"block", position:"absolute"};
+        }
+        var self = this;
+        return(
+                <div>
+                  <span onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} > <img src='http://webhelp.wimba.com/WP/v3_1/en/User_Guide/Content/Resources/Images/small_11_preferences_plus.jpg'/> </span>
+                  <div style = {display} className="row">
+                    <div className="col s12 m6" >
+                      <div className="card blue-grey darken-1">
+                        <div className="card-content white-text">
+                          <span className="card-title">{label}</span>
+                          <p>{content}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ) 
+  }
+}
+
+
+
+export default HoverInfo
