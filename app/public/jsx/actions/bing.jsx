25d41b95fdf5bf341e2c5149e16291937222cba1
Rico Chen <ricochenx@gmail.com>
2016-06-15 19:56:59 -0700

    [refactor] align bing action with reddit and twitter actions

--- app/public/jsx/actions/bing.jsx
+++ app/public/jsx/actions/bing.jsx
@@ -3,9 +3,10 @@ import axios from 'axios'
 export const FETCH_BING = 'FETCH_BING'
 
 export const fetchBing = (text) => {
-  const data = axios.post('/_api/bing/search', { "query": text });
+  const request = axios.post('/_api/bing/search', { "query": text });
+
   return {
     type: FETCH_BING,
-    payload: data
+    payload: request
   };
 }
