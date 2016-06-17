9fdb169dccac5b41d9ddfa040e8a4986a760ee66
Ben Chen <coben88@gmail.com>
2016-06-17 14:59:04 -0700

    metrics advacned

--- app/server/data/utils/sentimentAnalysis.js
+++ app/server/data/utils/sentimentAnalysis.js
@@ -30,12 +30,16 @@ function sentimentAnalyzer() {
     };
 
     const normal_dist_data_filter = (content) => {
+
         let normalized = content.map(function(curr, index, arr) {
             return curr[1];
         });
+
         normalized.sort();
+        var count = normalized.length;
         return {
             set: content,
+            setSize: normalized.length,
             mean: stats.mean(normalized),
             median: stats.median(normalized),
             standardDeviation: stats.stdev(normalized),
@@ -44,9 +48,16 @@ function sentimentAnalyzer() {
         };
     };
 
+    const additionalCalc = (content, rankings) =>{
+
+    };
+
     const analyze = (textObject) => {
         let analyzedResults = {};
         let rankingHolder = [];
+        let negativeSentiments = 0;
+        let positiveSentiments = 0;
+        let maxSentimentImpact = {actualTweet:'', sentimentStrength:0, votes:0, posNeg:""};
         for (var postKey in textObject) {
             let curr = textObject[postKey];
             let content = curr.content;
@@ -54,7 +65,23 @@ function sentimentAnalyzer() {
             	let currentSentiment = sentiment(content[contentKey].text);
                 if (currentSentiment.positive.length != 0 || currentSentiment.negative.length != 0) {
                     content[contentKey].sentiment = currentSentiment;
+                    
+                    if(currentSentiment<0)
+                    	negativeSentiments++
+                    if(currentSentiment>0)
+                    	positiveSentiments++
+
+
                     content[contentKey].sentiment.w_rank = ranking(content[contentKey], textObject[postKey].author);
+                    let rankProp = content[contentKey].sentiment.w_rank[1];
+                     if(maxSentimentImpact.sentimentStrength <rankProp){
+                    	console.log("----------",content[contentKey].sentiment.w_rank, content[contentKey].text, content[contentKey].vote_count, content[contentKey].sentiment)
+                    	maxSentimentImpact.sentimentStrength = content[contentKey].sentiment.w_rank[0]; 
+                    	maxSentimentImpact.actualTweet = content[contentKey].text;
+                    	maxSentimentImpact.votes = content[contentKey].vote_count;
+                    	maxSentimentImpact.posNeg = content[contentKey].sentiment<0?"Negative":"Positive"
+                    }
+
                     rankingHolder.push(content[contentKey].sentiment.w_rank);
                 }else{
                 	delete textObject[postKey].content;
@@ -63,11 +90,19 @@ function sentimentAnalyzer() {
         }
 
         let normalData = normal_dist_data_filter(rankingHolder);
+        console.log("RANKING HOLDER",rankingHolder, rankingHolder.length)
+		negativeSentiments	 = negativeSentiments/rankingHolder.length
+		positiveSentiments	 = positiveSentiments/rankingHolder.length
+		normalData.percentPositive = positiveSentiments;
+		normalData.percentNegative = negativeSentiments;
         let data_W_analysis = {
             data: textObject
         }
         for (let dataKey in normalData) {
             data_W_analysis[dataKey] = normalData[dataKey]
+        }
+        for (let dataKey in maxSentimentImpact) {
+            data_W_analysis[dataKey] = maxSentimentImpact[dataKey]
         }
         return data_W_analysis;
     };
