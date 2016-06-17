34185bd9dcf5621f26147fbcb08da544b123384a
Ben Chen <coben88@gmail.com>
2016-06-16 18:50:02 -0700

    metrics

--- app/public/jsx/components/App.jsx
+++ app/public/jsx/components/App.jsx
@@ -4,13 +4,10 @@ import React from 'react'
 import SearchBar from '../containers/SearchBar.jsx'
 import TwitterGraph from '../containers/TwitterGraph.jsx'
 import RedditGraph from '../containers/RedditGraph.jsx'
-import InitialGraph from '../containers/InitialGraph.jsx'
+import InitialGraph from '../containers/SentimentDistribution.jsx'
 import HoverInfo from '../containers/HoverInfo.jsx'
 import Metrics from '../containers/Metrics.jsx'
 
-const App = () => (
-  <div>
-    <HoverInfo />
 
 const App = () => (
   <div>
