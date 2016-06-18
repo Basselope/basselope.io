bea8ee49d1a710e5af140af6c88c03f28e48dee7
Rico Chen <ricochenx@gmail.com>
2016-06-18 10:08:39 -0700

    [refactor] query does not need to be a string

--- app/public/jsx/actions/twitter.jsx
+++ app/public/jsx/actions/twitter.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const FETCH_TWITTER = 'FETCH_TWITTER'
 
 export const fetchTwitter = (text) => {
-  const request = axios.post('/_api/twitter/search', { "query": text });
+  const request = axios.post('/_api/twitter/search', { query: text });
 
   return {
     type: FETCH_TWITTER,
