d2fc3e9cb33848b0def79691bd22b336f44dcab3
Lukas Welinder <Lukas@Welinder.Co>
2016-06-28 09:18:20 -0700

    [design] beginning implementation of 'd3Plot' interactivity

--- app/public/jsx/views/d3/d3Plot.jsx
+++ app/public/jsx/views/d3/d3Plot.jsx
@@ -5,34 +5,56 @@ const node = document.createElement('div');
 const width = 600,
       height = 300;
 
-const color = d3.scale.linear()
-  .range(['#f80', '#08f']); // '#f80', '#58b', '#08f', '#0f9'
-
-const map = {
-  x: (d, x) => (width / 2) + (x * (width / 2 / Math.min(...d3.extent(d,(val) => val[0]).map((v) => Math.abs(v))))),
-  y: (d, y) => height - ((height / d3.extent(d,(val) => val[1])[1]) * y),
-  r: (r) => 3 * Math.min((r * .9) + 3, 22),
-  c: (d, x) => color(map.x(d,x) / width)
-};
 
 const svg = d3.select(node).append('svg')
   .attr('width', width)
   .attr('height', height)
   .style('overflow', 'visible');
 
-const metric = {
-  mean: (d) => d3.mean(d,(val) => val[0]),
-  dev: (d) => d3.deviation(d,(val) => val[0])
+const Map = (d) => {
+  let xRange = d3.extent(d,(val) => val[0]);
+  let yRange = d3.extent(d,(val) => val[1]).map((v) => v < 0 ? 0 : v);
+
+  let xRangeMax = Math.max(...xRange.map((v) => Math.abs(v)));
+
+  let Mean = d3.mean(d,(val) => val[0]);
+  let Dev = d3.deviation(d,(val) => val[0]);
+
+  let X = (v) => d3.scale.linear().domain([-xRangeMax,xRangeMax]).range([0,width])(v);
+  let Y = (v) => d3.scale.pow().domain(yRange).range([height, 0])(Math.abs(v));
+  let R = (v) =>  3 + d3.scale.sqrt().domain(yRange).range([1,13])(Math.abs(v));
+  let Color = (v) => d3.scale.linear().domain([-xRangeMax,xRangeMax]).range(['#f80', '#08f'])(v);
+
+  return {
+    c: Color,
+    x: X,
+    y: Y,
+    r: R,
+
+    xRange: xRange,
+    xRangeMax: xRangeMax,
+    yRange: yRange,
+    mean: Mean,
+    dev: Dev
+  }
 };
 
-const label = (d) => {
-  let dev = map.x(d,metric.dev(d))/2;
-  let mean = map.x(d,metric.mean(d));
+const axis =
+  [{
+    icon: 'sentiment_dissatisfied',
+    x: -1
+  },{
+    icon: 'sentiment_satisfied',
+    x: 1
+  }];
 
+const metrics = (d,m) => {
+  let mean = m.x(m.mean);
+  let dev = Math.sqrt(m.x(m.dev))*2;
   return [{
     x: mean,
     y1: -50,
-    y2: height+150,
+    y2: height+70,
     class: 'plot-mean'
   },{
     x: mean - dev,
@@ -47,30 +69,36 @@ const label = (d) => {
   }]
 };
 
-function plot(d) {
-  // let xRange = d3.extent(d,(val) => val[0]);
-  // let xSpread = width / 2 / Math.max(...Math.abs.apply(xRange));
+function plot(d,m) {
   return d.map((val) => ({
-    c: map.c(d,val[0]),
-    x: map.x(d,val[0]),
-    y: map.y(d,val[1]),
-    r: map.r(val[1])
+    c: m.c(val[0]),
+    x: m.x(val[0]),
+    y: m.y(val[1]),
+    r: m.r(val[1])
   }));
 }
 
 function transition(count) {
-  d3.selectAll('circle')
+  d3.select('.plot-data').selectAll('circle')
     .transition()
     .delay((d,i) => (i * 33 + (count - i)))
     .duration(420)
     .each((d,i) => {
-      if(i === count-1)
-        d3.selectAll('line')
+      if(i === count-1) {
+
+        d3.select('.plot-metrics').selectAll('line')
           .transition()
           .duration(700)
           .attr('x1', (d) => d.x)
           .attr('x2', (d) => d.x)
-          .style('opacity', .5);
+          .style('opacity', .7);
+
+        d3.select('.plot-axis').selectAll('text')
+          .transition()
+          .duration(300)
+          .style('opacity', .7);
+
+      }
     })
     .attr('r', (d) => d.r)
     .attr('cy', (d) => d.y);
@@ -85,23 +113,36 @@ const createNode = function(...data) {
     return null; //document.createElement('div');
 
   let d = data.reduce((curr,val) => [].concat(curr,val.set), []);
-  let mean = map.x(d,metric.mean(d));
-  let dev = map.x(d,metric.dev(d));
+  let m = Map(d);
 
-  svg.selectAll('circle')
-    .data(plot(d))
+  svg.append('g')
+    .attr('class', 'plot-data')
+  .selectAll('circle')
+    .data(plot(d,m))
     .enter().append('circle')
     .attr('cx', (d) => d.x)
-    .attr('cy', height)
+    .attr('cy', (d,i) => height)
     .attr('r', 0)
     .style('fill', (d) => d.c)
-    .style('opacity', .7)
+    .style('opacity', .5)
     .on('mount', function() {
       transition(d.length);
+    })
+    .on('mouseenter', function(d) {
+      d3.select(this).transition().duration(400)
+        .attr('r',(d) => (d.r * 1.3))
+        .style('opacity',.7);
+    })
+    .on('mouseout', function(d) {
+      d3.select(this).transition().duration(300)
+        .attr('r',(d) => d.r)
+        .style('opacity',.5);
     });
 
-  svg.selectAll('line')
-    .data(label(d))
+  svg.append('g')
+    .attr('class', 'plot-metrics')
+  .selectAll('line')
+    .data(metrics(d,m))
     .enter().append('line')
     .attr('y1', (d) => d.y1)
     .attr('y2', (d) => d.y2)
@@ -110,7 +151,33 @@ const createNode = function(...data) {
     .attr("stroke", "gray")
     .attr("stroke-width", 3)
     .attr('class', (d) => d.class)
-    .style('opacity',1);
+    .style('opacity',0);
+
+  let axisGroup = svg.append('g')
+    .attr('class', 'plot-axis')
+    .attr('width', width)
+  .selectAll('text')
+    .data(axis).enter();
+
+  axisGroup.append('text')
+    .text((d) => d.icon)
+    .attr('y', height + 120)
+    .attr('x', (d) => m.x(d.x * m.xRangeMax))
+    .attr('font-family', 'Material Icons')
+    .attr('font-size', '42px')
+    .style("text-anchor", "middle")
+    .style('fill', (d) => d3.rgb(m.c(d.x)).darker(1.3))
+    .style('opacity', 0);
+
+  axisGroup.append('g:text')
+    .text((d) => (d.x ? `${d.x > 0 ? '+' : ''}${Math.round(d.x * m.xRangeMax * 100)}` : null))
+    .attr('y', height + 70)
+    .attr('x', (d) => m.x(d.x * m.xRangeMax))
+    .attr('font-family', 'Varela Round')
+    .attr('font-size', '19px')
+    .style("text-anchor", "middle")
+    .style('fill', (d) => d3.rgb(m.c(d.x)).darker(1.3))
+    .style('opacity', 0);
 
   return svg[0][0];
 };
