14275ac491e6319ddb1a1965f8e473e0828547af
Lukas Welinder <Lukas@Welinder.Co>
2016-06-29 11:33:44 -0700

    [refactor/design] labeling and refactor on time-line graph

--- app/public/jsx/views/d3/d3Time.jsx
+++ app/public/jsx/views/d3/d3Time.jsx
@@ -2,6 +2,9 @@ import d3 from 'd3'
 
 const node = document.createElement('div');
 
+const width = 700,
+      height = 300;
+
 function d3Time() {
 
   function prefix_postfix(dataContent) {
@@ -24,6 +27,11 @@ function d3Time() {
     };
     dataContent.unshift(lower);
     dataContent.push(top);
+  }
+
+  function formatDate(unformated) {
+    let theDate = unformated.years + "" + ('0' + unformated.months).slice(-2) + "" + ('0' + unformated.date).slice(-2);
+    return Number(theDate); //theDate.getYear() + "" + (theDate.getMonth() + 1) + "" + (theDate.getDay() + 1) + "" + theDate.getHours()
   }
 
   function posNegFilter(data) {
@@ -56,12 +64,6 @@ function d3Time() {
     }
   }
 
-
-  function formatDate(unformated) {
-    let theDate = unformated.years + "" + ('0' + unformated.months).slice(-2) + "" + ('0' + unformated.date).slice(-2);
-    return Number(theDate); //theDate.getYear() + "" + (theDate.getMonth() + 1) + "" + (theDate.getDay() + 1) + "" + theDate.getHours()
-  }
-
   function combineData(dataSet) {
     let newObject = {};
     let newObjectCount = {};
@@ -88,22 +90,10 @@ function d3Time() {
     return data;
   }
 
-
-  var svg = d3.select(node).append("svg")
-    .attr("width", 600)
-    .attr("height", 300);
-
-  const createNode = function(...passedData) {
-
-    let resolved = false;
-    if (passedData)
-      resolved = passedData.reduce((curr, val) => curr ? val.hasOwnProperty('set') : false, true);
-    if (!resolved)
-      return null; // = passedData.reduce((curr,val) => [].concat(curr,val.data), []);
+  function parseData(passedData) {
     let data = [];
-    let dataObject = {};
-
-    let ranges = {}
+    // let dataObject = {};
+    // let ranges = {}
     for (var dataSources in passedData) {
       let d = passedData[dataSources].data
       for (var postKey in d) {
@@ -122,39 +112,80 @@ function d3Time() {
         }
       }
     }
-    prefix_postfix(data);
-    let seperatedData = posNegFilter(data);
+    return prefix_postfix(data);
+  }
+
+  const svg = d3.select(node).append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .style('overflow', 'visible');
+
+  const createNode = function(...data) {
+
+    let resolved = false;
+    if (data)
+      resolved = data.reduce((curr, val) => curr ? val.hasOwnProperty('set') : false, true);
+    if (!resolved)
+      return null; // = passedData.reduce((curr,val) => [].concat(curr,val.data), []);
+
+
+    let d3Data = parseData(data);
+    let seperatedData = posNegFilter(d3Data);
     let positiveData = combineData(seperatedData.positive);
     let negativeData = combineData(seperatedData.negative);
+    console.log(seperatedData);
 
-    ranges.yMin = Math.min.apply(Math, negativeData.map(function(o) {
-      return o.avgScore;
-    })); // (negativeData[1].avgScore)
-    ranges.yMax = Math.max.apply(Math, positiveData.map(function(o) {
-      return o.avgScore;
-    })); // (positiveData[positiveData.length-2].avgScore)
+    // let yMin = Math.min.apply(Math, negativeData.map(function(o) {
+    //   return o.avgScore;
+    // })); // (negativeData[1].avgScore)
+    // let yMax = Math.max.apply(Math, positiveData.map(function(o) {
+    //   return o.avgScore;
+    // })); // (positiveData[positiveData.length-2].avgScore)
+
+    let yMin = d3.min(negativeData,(d) => d.avgScore);
+    let yMax = d3.max(positiveData,(d) => d.avgScore);
 
     console.log("POSITIVEDATA", positiveData, seperatedData.positive);
     console.log("NEGATIVEDATA", negativeData, seperatedData.negative);
-    //console.log(ranges)
-    var m = [80, 80, 80, 80];
+
+    // var m = [80, 80, 80, 80];
     var w = 600;
     var h = 300;
-    console.log("RANGES", ranges);
 
-    let yMaxRange = Math.max(Math.abs(ranges.yMin), ranges.yMax);
-    const color = d3.scale.linear().domain([ranges.yMin, ranges.yMax]).range(['#f80', '#08f']);
+    let yMaxRange = Math.max(Math.abs(yMin), yMax);
+
+    // const color = d3.scale.linear().domain([ranges.yMin, ranges.yMax]).range(['#f80', '#08f']);
     var x = d3.scale.linear().domain([0, positiveData.length]).range([0, w]);
     var y = d3.scale.linear().domain([-yMaxRange, yMaxRange]).range([h, 0]);
-    var line = d3.svg.line()
-      .x(function(d, i) {
-        return x(i); //TODO: Here it only iterates on index, not date. s
-      })
-      .y(function(d) {
-        return y(d.avgScore);
-      }).interpolate("basis");
 
-    let path = line(positiveData) + line(negativeData);
+
+    // var line = d3.svg.line()
+    //   .x(function(d, i) {
+    //     return x(i); //TODO: Here it only iterates on index, not date. s
+    //   })
+    //   .y(function(d) {
+    //     return y(d.avgScore);
+    //   }).interpolate("basis");
+
+    let tics = Math.max(positiveData.length,negativeData.length);
+
+    let max = d3.svg.line()
+      .x((d,i) => x(i))
+      .y(y(yMax))(Array(tics));
+
+    let min = d3.svg.line()
+      .x((d,i) => x(i))
+      .y(y(yMin))(Array(tics));
+
+    let area = d3.svg.area()
+      .x((d,i) => x(i))
+      .y0((d,i) => y(negativeData[i].avgScore))
+      .y1((d,i) => y(positiveData[i].avgScore))
+      .interpolate('basis')(Array(tics));
+
+
+
+    // let path = area(positiveData); //line(positiveData) + line(negativeData);
 
     var gradient = svg.append("svg:defs")
       .append("svg:linearGradient")
@@ -177,9 +208,32 @@ function d3Time() {
       .attr("stop-color", "#08f")
       .attr("stop-opacity", 1);
 
-    svg.append('g').attr('fill', 'url(#gradient)')
-      .append('path')
-      .attr('d', path);
+    let graph = svg.append('g').attr('class', 'time-line');
+
+    graph.append('path')
+      .attr('d', area)
+      .attr('fill', 'url(#gradient)');
+
+    graph.append('line')
+      .attr('x1', -20)
+      .attr('x2', width + 20)
+      .attr('y1', y(yMax))
+      .attr('y2', y(yMax))
+      .attr('stroke', '#333')
+      .attr('stroke-width', 2)
+      .style('opacity', .5);
+
+    graph.append('line')
+      .attr('x1', -20)
+      .attr('x2', width + 20)
+      .attr('y1', y(yMin))
+      .attr('y2', y(yMin))
+      .attr('stroke', '#333')
+      .attr('stroke-width', 2)
+      .style('opacity', .5);
+    // svg.select('g.time-line').append
+
+
     // svg.append("path").attr("d", line(positiveData))
     //   .style('fill', 'url(#gradient');
     // svg.append("path").attr("d", line(negativeData))
